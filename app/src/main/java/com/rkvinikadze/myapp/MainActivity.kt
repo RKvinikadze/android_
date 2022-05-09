1d1bffbadd9aac6740367d360ae54f71f4ca9c48
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-05-09 03:48:00 -0700

    final commit

--- app/src/main/java/com/rkvinikadze/myapp/MainActivity.kt
+++ app/src/main/java/com/rkvinikadze/myapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.rkvinikadze.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+
+const val MESSAGE_KEY = "extra_message"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.userImg).setOnClickListener{
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.imageButton).setOnClickListener{
+            val intent = Intent(this, UserActivity::class.java)
+                .putExtra(MESSAGE_KEY, findViewById<EditText>(R.id.editText).text.toString())
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/rkvinikadze/myapp/UserActivity.kt
+++ app/src/main/java/com/rkvinikadze/myapp/UserActivity.kt
@@ -1,11 +1,32 @@
 package com.rkvinikadze.myapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class UserActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        val msg = if (intent.getStringExtra(MESSAGE_KEY) == null) "" else intent.getStringExtra(MESSAGE_KEY)
+
+        if (msg != "")
+            findViewById<TextView>(R.id.profile_text).text = "Hello, $msg"
+
+        findViewById<ImageButton>(R.id.to_main).setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
